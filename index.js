ff3325db0581fcbc637dcdbafa00eeec900b5f5f
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-11-06 16:35:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({ extended: false }))
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -15,28 +17,28 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-let url;
-let randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-const urlChecker = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
-
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-app.post('api/shorturl', (req, res) => {
-  url = req.body.url;
-  if(url.match(urlChecker)){
-    res.json({original_url: url, short_url: randomNumber});
+let randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+let reqUrl
+const urlChecker = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+
+app.post('/api/shorturl', function (req, res) {
+  reqUrl = req.body.url
+  if (reqUrl.match(urlChecker)) {
+    res.json({ original_url: reqUrl, short_url: randomNumber })
   }
   res.json({ error: 'invalid url' })
 })
 
-app.get('api/shorturl/' + randomNumber, (req, res) => {
-  res.redirect(url)
+app.get('/api/shorturl/' + randomNumber, function (req, res) {
+  res.redirect(reqUrl)
 })
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
